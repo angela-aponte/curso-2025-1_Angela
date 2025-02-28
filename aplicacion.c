9969e94ae359bb96c75d9c889a310acbe292932d
Ubuntu <angela.aponte01@uceva.edu.co>
2025-02-28 04:42:08 +0000

    conexion

---
+++ aplicacion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcionamiento del editor NANO
+//Guarda con control O (enter)
+//Sale con control x
+//Arreglo de strings: char**
+
+//La funcion main es el entry point (punto de entrada) de un ejecutable de lenguaje C
+int main(void){
+   printf("\nHola Mundo!\n");
+   return 0;
+}
